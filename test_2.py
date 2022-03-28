38a2cd843985fc6e9572444bde27d217e677ba96
Albadit <46565849+Albadit@users.noreply.github.com>
2022-03-28 12:24:10 +0200

    fix

---
+++ test_2.py
@@ -0,0 +1,73 @@
+###### Test 2
+#### BA 1
+# def reverse(n):
+#   rev = str(n)[::-1]
+#   rev = int(rev)
+#   return rev
+
+# def isPalindrome(n):
+#   if n == reverse(n):
+#     return True
+#   else:
+#     return False
+
+# n = 123
+# a = reverse(n)
+# b = isPalindrome(n)
+# print(a,b)
+
+# #### BA 2
+# def sumUpTo(n):
+#   global point
+#   point += n 
+#   n -= 1
+#   if n > 0:
+#     sumUpTo(n)
+#   elif n <= 0: 
+#     return 0
+#   return point
+
+# point = 0
+# a = 5
+# result = sumUpTo(a)
+
+# print(result)
+
+# L = [i for i in range(10,30,10)] * 2
+# print(L)
+
+# def repeat(n, sym):
+#   text = ''
+#   for i in range(n):
+#     text += sym
+#   return text
+
+# def line(n):
+#   sym = '*'
+#   return repeat(n, sym)
+
+# def square(n):
+#   return repeat(n, line(n) + '\n')
+
+# def rectangle(width, height):
+#   return repeat(height , line(width) +  '\n')
+
+# s = square(5) 
+# r = rectangle(5, 3)
+
+# print(r)
+
+def f():
+  global x
+  x = x // 2
+
+def g():
+  global x
+  x = x + 1
+
+x = 11
+while x > 1:
+  if x % 2 == 0:
+    f()
+  else:
+  	g()
